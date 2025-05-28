3c069b2d9660d1680b8375fa74e01b335dfb4431
stevo1310 <stevo1310@yahoo.com>
2025-05-28 17:05:13 -0500

    20250528_Course_Progress_1

--- Start/Ch2/variables_start.py
+++ Start/Ch2/variables_start.py
@@ -10,12 +10,34 @@ mystr = "This is a string"
 mybool = True
 
 # We can display the content of a variable using the print() function
+print(myint)
+print(myfloat)
+print(mystr)
+print(mybool)
 
 
 # Operators are used to perform operations on variables
+print(myint + myfloat)
+print(myint * myfloat)
+print(myint / myfloat)
+print(myint % 3)
 
+another_stir = "This is another string"
+print(mystr + " " + another_stir)
+print("nom "* 3)
 
 # Logical and comparison operators 
+print(myint == 10)
+print(myfloat != 20)
+print(myint > 20)
+print(myint < 10)
 
+print(myint < 5 and myfloat > 25.0)
+print(myint < 5 or myfloat > 25.0)
+print(not(myint < 5 or myfloat > 25.0))
 
 # re-declaring a variable works
+
+myint = 26
+
+
